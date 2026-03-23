f4a515877ff18087ed1749708eff4e90f7e4f098
Bagirov24 <79216053+Bagirov24@users.noreply.github.com>
2026-03-23 12:54:50 +0300

    feat: add SnackbarProvider from notistack to main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
+import { SnackbarProvider } from 'notistack'
 import theme from '@/lib/theme'
 import App from './App'
 import './index.css'
@@ -18,11 +19,17 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        autoHideDuration={3000}
+      >
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </SnackbarProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
