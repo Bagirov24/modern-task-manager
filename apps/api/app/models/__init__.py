50f11d1dd48e423c0056206ec8bf1eb1caef5195
Bagirov24 <79216053+Bagirov24@users.noreply.github.com>
2026-03-24 17:25:50 +0300

    fix: add Notification model to models/__init__.py

--- apps/api/app/models/__init__.py
+++ apps/api/app/models/__init__.py
@@ -3,3 +3,4 @@ from app.models.task import Task as Task
 from app.models.project import Project as Project
 from app.models.comment import Comment as Comment
 from app.models.label import Label as Label
+from app.models.notification import Notification as Notification
